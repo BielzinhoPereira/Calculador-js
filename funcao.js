9091d3f5c10edd698675ab1a3775d2b4694d4f60
BielzinhoPereira <gabriel.pereira_rodrigues@outlook.com>
2023-09-14 00:53:43 -0300

    1 commit, calculadora com erro(descobrir erro)

---
+++ funcao.js
@@ -0,0 +1,19 @@
+// tipos de funcoes
+//declarativas 
+/*function funcao (){
+    console.log ('sou uma mensagem de uma funcao declarativa');
+}
+funcao();*/
+// expressoes de funcoes = com nomeação 
+/*var funcao = function  funcao (){
+    console.log('sou uma mensagem de funcao de expresao!');
+}
+funcao();*/
+
+// arrow function = funcao expressada em sintaxe curta, sempre anonimas
+
+var funcao = () => {
+       console.log(' sou uma arrow');
+}
+funcao();
+
---
+++ tiposDeFuncao.js
@@ -0,0 +1,32 @@
+// tipos de funcoes
+//declarativas 
+/*function funcao (){
+    console.log ('sou uma mensagem de uma funcao declarativa');
+}
+funcao();*/
+// expressoes de funcoes = com nomeação 
+/*var funcao = function  funcao (){
+    console.log('sou uma mensagem de funcao de expresao!');
+}
+funcao();*/
+
+// arrow function = funcao expressada em sintaxe curta, sempre anonimas
+
+/*var funcao = () => {
+    console.log(' sou uma arrow');
+}
+funcao();*/
+
+// funcoes aritimeticas
+
+// number () = converter valores em numeros
+
+// prompt() = para registrar entrada de usuarios
+
+// alert() para mostrar mensagem ao usuario 
+
+//templateStrings para usar string junto com expressao
+
+
+
+
